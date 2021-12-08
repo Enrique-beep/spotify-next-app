390f8fe8d0cb2583ad399c9e472c972f5074efa7
Enrique Contreras <contrerasenrique973@gmail.com>
2021-12-08 15:06:53 -0700

    porject init

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+export default function Home() {
+  return (
+    <div className="">
+      <h1>Spotify</h1>
+
+      <main>
+        {/* Siderbard */}
+        {/* Center */}
+      </main>
+
+      <div>
+        {/* Player */}
+      </div>
+    </div>
+  )
+}
