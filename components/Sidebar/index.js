03d8543f01c2fd2c00b7f87fe8186f7bb528f8bd
Enrique Contreras <contrerasenrique973@gmail.com>
2021-12-15 16:03:57 -0700

    player #31105

--- components/Sidebar/index.js
+++ components/Sidebar/index.js
@@ -1,3 +1,5 @@
+import { playlistIdState } from '@/atoms/playlistAtom'
+import useSpotify from '@/hooks/useSpotify'
 import {
   HomeIcon,
   SearchIcon,
@@ -6,10 +8,26 @@ import {
   HeartIcon,
   RssIcon
 } from '@heroicons/react/outline'
+import { useSession } from 'next-auth/react'
+import { useEffect, useState } from 'react'
+import { useRecoilState } from 'recoil'
 
 function Sidebard() {
+  const spotifyApi = useSpotify()
+  const { data: session, status } = useSession()
+  const [playlists, setPlaylists] = useState([])
+  const [playlistId, setPlaylistId] = useRecoilState(playlistIdState)
+
+  useEffect(() => {
+    if (spotifyApi.getAccessToken()) {
+      spotifyApi.getUserPlaylists().then((data) => {
+        setPlaylists(data.body.items)
+      })
+    }
+  }, [session, spotifyApi])
+
   return (
-    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+    <div className="text-gray-500 p-5 text-xs lg:text-sm border-r border-gray-900 overflow-y-scroll scrollbar-hide h-screen sm:max-w-[12rem] lg:max-w-[15rem] hidden md:inline-flex">
       <div className="space-y-4">
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
@@ -46,7 +64,15 @@ function Sidebard() {
         <hr className="border-t-[0.1px] border-gray-900" />
 
         {/* Playlist... */}
-        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        {playlists.map((playlist) => (
+          <p
+            key={playlist.id}
+            onClick={() => setPlaylistId(playlist.id)}
+            className="cursor-pointer hover:text-white"
+          >
+            {playlist.name}
+          </p>
+        ))}
       </div>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import Center from '@/components/Center'
 import Sidebard from '@/components/Sidebar'
+import Player from '@/components/Player'
+import { getSession } from 'next-auth/react'
 
 export default function Home() {
   return (
     <div className="bg-black h-screen overflow-hidden">
-      <main className="">
+      <main className="flex">
         <Sidebard />
-        {/* Center */}
+        <Center />
       </main>
 
-      <div>{/* Player */}</div>
+      <div className='sticky bottom-0'>
+        <Player />
+      </div>
     </div>
   )
+}
+
+export async function getServerSideProps(context) {
+  const session = await getSession(context)
+
+  return {
+    props: {
+      session
+    }
+  }
 }
